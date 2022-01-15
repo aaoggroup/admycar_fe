4abb7e76dd01bd395798ff65acc9c7e4f8b16a7c
Aviad Shmuel <vocaviad@gmail.com>
2022-01-16 01:25:32 +0200

    added text to homepage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import "./homepage.css";
 
 function HomePage() {
   return (
     <div className="col-12">
+      <div className="animation-container">
+        <h1 class="alt">
+          We are driving already, let's make some money.
+          <br />
+        </h1>
+        <p class="fadeIn wait-7s">Join our growing platform.</p>
+      </div>
       <video
         className="col-12"
         src="/videos/home_video.mp4"
