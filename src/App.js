7371356283e823c05fec71be827e6d0237da3e1b
Aviad Shmuel <vocaviad@gmail.com>
2022-01-15 13:16:17 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>Admycar</div>
+    </div>
+  );
+}
+
+export default App;
